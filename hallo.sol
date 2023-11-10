dd9454e0ad3858ee7450021d0fb3dfdf8f7c913a
nhysagabrelia <150423646+nhysagabrelia@users.noreply.github.com>
2023-11-10 16:47:21 +0800

    Create hallo.sol

---
+++ hallo.sol
@@ -0,0 +1,14 @@
+// Contoh Solidity Contract
+pragma solidity ^0.8.0;
+
+contract HelloWorld {
+    string public greeting;
+
+    constructor() {
+        greeting = "Hello, World!";
+    }
+
+    function setGreeting(string memory _greeting) public {
+        greeting = _greeting;
+    }
+}
